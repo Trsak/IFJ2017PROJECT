311bda44d26079749ddcd8d9d975358e52d96106
Roman Bártl <bbartl.roman@gmail.com>
2017-10-15 16:16:17 +0200

    Making built_in header file

---
+++ src/built_in.c
@@ -0,0 +1,38 @@
+/**
+ * @file built_in.c
+ * @author Roman Bartl (xbartl06)
+ * @brief Built in functions of IFJ17 language
+ */
+
+
+#include "<stdio.h>"
+#include "<stdlib.h>"
+
+
+void print( char** stringsToPrint ) {
+
+}
+
+char* input ( ) {
+    return "Hello world";
+}
+
+
+int stringLength ( char* string ) {
+    return 0;
+}
+
+
+char* subStr ( char* string, int startChar, int stringLength ) {
+    return "Hello world";
+}
+
+
+int asc ( char* string, int charPosition ) {
+    return 0;
+}
+
+
+char chr ( int asciiVal ) {
+    return 'a';
+}
