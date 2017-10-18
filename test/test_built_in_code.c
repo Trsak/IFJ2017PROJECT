bb8f13037a6bf6a4867963b063e842f3f9db1518
Petr Šopf <trsak1@seznam.cz>
2017-10-18 14:00:04 +0200

    Create test structure for builtin functions

---
+++ test/test_built_in_code.c
@@ -0,0 +1,13 @@
+#include "ProductionCode.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(BuiltInCode);
+
+TEST_SETUP(ProductionCode)
+        {
+        }
+
+TEST_TEAR_DOWN(ProductionCode)
+        {
+        }
