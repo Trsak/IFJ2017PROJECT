edde3b56e22e0f67f49d2ad642dbf4ac1c9dcfee
Petr Šopf <trsak1@seznam.cz>
2017-10-18 18:15:59 +0200

    Implemented subStr function

--- test/test_built_in.c
+++ test/test_built_in.c
@@ -19,7 +19,7 @@ void test_built_in_asc(void) {
     char str[] = "Testovaci";
 
     TEST_ASSERT_EQUAL_INT(84, asc(str, 0));
-    TEST_ASSERT_EQUAL_INT(105, asc(st, 8));
+    TEST_ASSERT_EQUAL_INT(105, asc(str, 8));
     TEST_ASSERT_EQUAL_INT(65, asc("AB", 0));
     TEST_ASSERT_EQUAL_INT(66, asc("AB", 1));
     TEST_ASSERT_EQUAL_INT(0, asc(str, -1));
@@ -37,6 +37,54 @@ void test_built_in_subStr(void) {
     test = subStr(str, 0, 5);
     TEST_ASSERT_EQUAL_STRING("", test);
     free(test);
+
+    test = subStr(str, -2, 5);
+    TEST_ASSERT_EQUAL_STRING("", test);
+    free(test);/**
+
+    test = subStr("", 1, 1);
+    TEST_ASSERT_EQUAL_STRING("", test);
+    free(test);
+
+    test = subStr(str, 1, 0);
+    TEST_ASSERT_EQUAL_STRING("", test);
+    free(test);
+
+    test = subStr(str, 1, 0);
+    TEST_ASSERT_EQUAL_STRING("", test);
+    free(test);
+
+    test = subStr(str, 1, -2);
+    TEST_ASSERT_EQUAL_STRING("Testovaci", test);
+    free(test);
+
+    test = subStr(str, 3, -2);
+    TEST_ASSERT_EQUAL_STRING("stovaci", test);
+    free(test);
+
+    *test = subStr(str, 1, 50);
+    TEST_ASSERT_EQUAL_STRING("Testovaci", test);
+    free(test);
+
+    test = subStr(str, 3, 50);
+    TEST_ASSERT_EQUAL_STRING("stovaci", test);
+    free(test);
+
+    test = subStr(str, 3, 1);
+    TEST_ASSERT_EQUAL_STRING("s", test);
+    free(test);
+
+    test = subStr(str, 3, 2);
+    TEST_ASSERT_EQUAL_STRING("st", test);
+    free(test);
+
+    test = subStr(str, 1, 2);
+    TEST_ASSERT_EQUAL_STRING("Te", test);
+    free(test);
+
+    char *test = subStr(str, 4, 4);
+    TEST_ASSERT_EQUAL_STRING("tova", test);
+    free(test);**/
 }
 
 int main(void) {
