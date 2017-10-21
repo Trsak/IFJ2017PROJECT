fb0216d7a48f6bbad3e5ea9a3be47de814449846
Roman Bártl <bbartl.roman@gmail.com>
2017-10-21 13:50:39 +0200

    Parsing functions only - no derivation tree, only array

---
+++ src/parser.c
@@ -0,0 +1,452 @@
+/**
+ * @file parser.c
+ * @author Roman Bartl (xbartl06)
+ * @brief TODO
+ */
+
+
+#include "parser.h"
+
+//static int tokens[50] = {DECLARE, FUNCTION, ID, BRACKET_LEFT, ID, AS, INTEGER, BRACKET_RIGHT, AS, DOUBLE, EOL};
+//static int tokens[50] = {DECLARE, FUNCTION, ID, BRACKET_LEFT, BRACKET_RIGHT, AS, DOUBLE, EOL};
+//static int tokens[50] = {DECLARE, FUNCTION, ID, AS, DOUBLE, EOL};
+
+//static int tokens[50] = {FUNCTION, ID, AS, DOUBLE, EOL, END, FUNCTION, EOL};
+
+static int tokens[50] = {DECLARE, FUNCTION, ID, BRACKET_LEFT, ID, AS, INTEGER, COMMA, ID, AS, DOUBLE, AS, STRING, EOL};
+
+static int tree[50];
+static int last = -1;
+
+int getNextToken() {
+    static int id = -1;
+
+    id++;
+
+    return tokens[id];
+}
+
+
+bool program() {
+    int token = getNextToken();
+
+    if(function_follow(token)) {     // block 'functions' is empty (epsilon)
+        //TODO - add to derivation tree
+
+        last++;
+        tree[last] = token;
+
+
+    } else {
+        if(!function_first(token)) {
+            print_err_msg(ERROR_SYNTAX, "");  //TODO - add err msg
+
+            return false;
+        }
+
+        //TODO - add to derivation tree
+
+        last++;
+        tree[last] = token;
+
+
+        if(!functions())
+            return false;
+    }
+
+
+    if(main_body()) printf("\ntrue\n");
+
+    return true;
+}
+
+
+bool function_first(int token) {
+    if (token == DECLARE || token == FUNCTION)
+        return true;
+
+    return false;
+}
+
+
+bool functions() {
+
+    int token = tree[last]; // TODO get last added token from tree and compare it
+
+    if(token != DECLARE && token != FUNCTION)
+        token = getNextToken();
+
+
+    if(token == DECLARE) { // TODO skip if token is in the tree
+        //TODO - add token to tree
+
+        //last++;
+        //tree[last] = token;
+
+        if(!function_header())
+            return false;
+
+        if(!function_next())
+            return false;
+
+        return true;
+
+    } else if (token == FUNCTION) { // TODO skip if token is in the tree
+
+        if(!function_header())
+            return false;
+
+        if(!statement())
+            return false;
+
+        if(!function_end())
+            return false;
+
+        if(!function_next())
+            return false;
+
+        return true;
+    }
+
+
+    return false;
+}
+
+
+bool function_header() {
+    int token;
+
+    if(tree[last] != FUNCTION) {
+        token = getNextToken();
+
+        if (token != FUNCTION) { // TODO skip this condition if token is in the tree
+            print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+            return false;
+        }
+
+        //TODO add token into the tree
+        last++;
+        tree[last] = token;
+
+    }
+
+
+    token = getNextToken();
+
+    if(token != ID) {
+        print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+        return false;
+    }
+
+    //TODO add token into the tree
+    last++;
+    tree[last] = token;
+
+    token = getNextToken();
+
+    if(!function_as_first(token)) {
+        if (!function_it_first(token)) {
+            print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+            return false;
+        }
+
+        // TODO add token into tree
+        last++;
+        tree[last] = token;
+
+        if(!function_it())
+            return false;
+
+    } else {
+
+        last++;
+        tree[last] = token;
+    }
+
+    if(!function_as())
+        return false;
+
+
+    token = getNextToken();
+
+
+    if(token != EOL) {
+        print_err_msg(ERROR_SYNTAX, "");
+        return false;
+    }
+
+    last++;
+    tree[last] = token;
+
+    return true;
+}
+
+bool function_it() {
+    //TODO - is this necessary?
+    // if(getNextToken() == BRACKET_LEFT); // TODO - if token is in tree, skip this condition
+
+
+    if(!declare_params()) {
+        return false;
+    }
+
+    if(tree[last] == BRACKET_RIGHT)
+        return true;
+
+    int token = getNextToken();
+
+    if(token != BRACKET_RIGHT){
+        print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+        return false;
+    }
+
+    last++;
+    tree[last] = token;
+
+    return true;
+}
+
+bool function_it_first(int token) {
+    if(token == BRACKET_LEFT)
+        return true;
+
+    return false;
+}
+
+bool function_as_first(int token) {
+    if(token == AS)
+        return true;
+
+    return false;
+}
+
+
+bool function_as() {
+
+    if(tree[last] != AS) {
+        int token = getNextToken();
+
+        if (token != AS) {
+            print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+            return false;
+        } //TODO - if token is already in tree, skip this condition
+
+        last++;
+        tree[last] = token;
+
+    }
+
+    if(!data_type())
+        return false;
+
+    return true;
+}
+
+bool function_next() {
+
+
+    int token = getNextToken();
+
+    if(function_follow(token)) {
+        //TODO - add into tree
+        last++;
+        tree[last] = token;
+
+        return true;
+    }
+
+    if(!functions())
+        return false;
+    /*
+    if(!function_first(token)) {
+        print_err_msg(ERROR_SYNTAX, "");  //TODO - add err msg
+
+        return false;
+    }
+
+    //TODO - add to derivation tree
+    last++;
+    tree[last] = token;
+
+    if(!functions())
+        return false;
+    */
+
+    return true;
+}
+
+bool function_end() {
+    int token = getNextToken();
+
+    if(token != END) {
+        print_err_msg(ERROR_SYNTAX, "");
+        return false;
+    }
+
+    //TODO - add to derivation tree
+    last++;
+    tree[last] = token;
+
+    token = getNextToken();
+
+    if(token != FUNCTION) {
+        print_err_msg(ERROR_SYNTAX, "");
+        return false;
+    }
+
+    //TODO - add to derivation tree
+    last++;
+    tree[last] = token;
+
+    token = getNextToken();
+
+    if(token != EOL) {
+        print_err_msg(ERROR_SYNTAX, "");
+        return false;
+    }
+
+    last++;
+    tree[last] = token;
+
+    return true;
+}
+
+
+bool function_follow(int token) {
+    if(token == SCOPE)
+        return true;
+
+    return false;
+}
+
+/*bool function_follow(int token) {
+    if(token == DIM || token == STATIC || token == SHARED ||
+            token == ID || token == PRINT || token == INPUT || token == DO || token == IF);
+    return true;
+}*/
+
+
+bool declare_params() {
+    int token = getNextToken();
+
+    //TODO - if last added is comma, skip this condition
+    if(tree[last] != COMMA) {
+        if (declare_params_follow(token)) {
+            //TODO - add to derivation tree
+
+            last++;
+            tree[last] = token;
+            return true;
+        }
+    }
+
+    if(token != ID) {
+        print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+        return false;
+    }
+
+    last++;
+    tree[last] = token;
+
+    if(!function_as()) {
+        return false;
+    }
+
+    if(!declare_params_next()) {
+        return false;
+    }
+
+    return true;
+}
+
+bool declare_params_next() {
+    int token = getNextToken();
+
+    if(declare_params_follow(token)) {
+        //TODO - add to derivation tree
+
+        last++;
+        tree[last] = token;
+        return true;
+    }
+
+    if(token != COMMA) {
+        print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+        return false;
+    }
+
+    last++;
+    tree[last] = token;
+
+    if(!declare_params())
+        return false;
+
+    return true;
+}
+
+bool declare_params_follow(int token) {
+    if(token == BRACKET_RIGHT)
+        return true;
+
+    return false;
+}
+
+
+bool data_type() {
+    int token = getNextToken();
+
+    if(token != INTEGER && token != DOUBLE && token != STRING) {
+        print_err_msg(ERROR_SYNTAX, ""); //TODO - err msg
+        return false;
+    }
+
+    //TODO - add into the tree
+    last++;
+    tree[last] = token;
+
+    return true;
+}
+
+bool statement() {
+    return true;
+}
+
+
+
+
+bool main_body(int token) { //TODO - find if token is already in derivation tree, then skip conditions
+    printf("Ahoj\n");
+
+    /*if(main_body_it());
+    if(statement());
+    if(end());
+    if(main_body_it());
+    */
+    return true;
+}
+
+/*
+bool main_body_it() {
+    if(getNextToken() == SCOPE);
+    if(getNextToken() == EOL);
+    return true;
+}
+
+bool statement() {
+    return true;
+}
+
+bool end_token() {
+    if (getNextToken() == END) {
+        // TODO insert into derivation tree
+        return true;
+    }
+
+    return false;
+}
+
+//bool eol_token() {
+    //if (getNextToken() == EOL) TODO
+  //  return true;
+//}
+*/
---
+++ src/parser.h
@@ -0,0 +1,55 @@
+/**
+ * @file parser.h
+ * @author Roman Bartl (xbartl06)
+ * @brief  TODO
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <malloc.h>
+#include "scanner.h"
+#include "lexems.h"
+#include "derivation_tree.c"
+#include "error_codes.c"
+
+
+bool program();
+
+bool functions();
+
+bool main_body();
+
+bool main_body_it();
+
+bool statement();
+
+bool function_follow(int token);
+
+bool function_first(int token);
+
+bool function_header();
+
+bool function_it();
+
+bool function_it_first(int token);
+
+bool function_as_first(int token);
+
+bool function_as();
+
+bool function_next();
+
+bool function_end();
+
+bool declare_params();
+
+bool declare_params_next();
+
+bool declare_params_follow(int token);
+
+bool data_type();
+
+bool statement();
+
+bool main_body();
