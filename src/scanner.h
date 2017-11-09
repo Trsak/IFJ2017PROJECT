a11a83bfcf33c8f8ee2fffe39d0f4f2b29243523
B4rtosek <B4rtosek@gmail.com>
2017-11-09 19:23:18 +0100

    Lexical Analysis update, bug fixes

--- src/scanner.h
+++ src/scanner.h
@@ -11,27 +11,34 @@
 
 typedef enum
 {
-	ID,
-	NUMBER,
+	ID, // _ or ABC_abc95
+	NUMBER, // int value 123456
+	DECIMAL_NUMBER, // float value 123.456 or 123e456 or 123.456E-789
+	STRING_EXPRESSION, // Has to start: !"my string" .. returns only my string.
+
 	PLUS, // +
 	MINUS, // -
 	MULTIPLY, // *
 	DIVISION, // /
-	INCREMENT, // ++
-	DECREMENT, // --
+	BACKSLASH, // \.
+
+	PLUS_ASSIGNMENT, // +=
+	MINUS_ASSIGNMENT, // -=
+	MULTIPLY_ASSIGNMENT, // *=
+	DIVISION_ASSIGNMENT, // /=
+	BACKSLASH_ASSIGNMENT, // \=
+
 	ASSIGNMENT, // =
 	LESS, // <
 	LESS_EQUAL, // <=
 	GREATER, // >
 	GREATER_EQUAL, // >=
 	NOT_EQUAL ,// <>
-	DECIMAL_NUMBER, // 123.456 or 123e456 or 123.456E-789
-	STRING_EXPRESSION, // Has to start: !"my string" .. returns only my string.
+
 	BRACKET_LEFT, // (
 	BRACKET_RIGHT, // )
 	COMMA, // ,
-	BACKSLASH, // backslash
-	SEMICOLON, // ;
+	SEMICOLON // ;
 } lexems;
 
 typedef enum
@@ -70,72 +77,11 @@ typedef enum
 	OR,
 	SHARED,
 	STATIC,
-	TRUE,
+	TRUE
 } keywords;
 
 #define EOL '\n'
 #define EOF '\0'
-
-/*
-// Lexems
-#define ID 0
-#define NUM 1
-#define PLUS 2 // +
-#define MINUS 3 // -
-#define MUL 4 // *
-#define DIV 5 // /
-#define INC 6 // ++
-#define DEC 7 // --
-#define ASSIGNMENT 8 // =
-#define LEQ 9 // <=
-#define NEQ 10 // <>
-#define LTN 11 // <
-#define DECIMAL_NUM 12 // 123.456 or 123e456 or 123.456E-789
-#define STRING_EXPRESSION 13 // Has to start: !"my string" .. returns only my string.
-#define BRACKET_LEFT 14 // (
-#define BRACKET_RIGHT 15 // )
-#define COMMA 16 // ,
-#define BACK_DIV 17 // backslash
-#define SEMICOLON 18 // ;
-
-// Keywords
-#define AS 20
-#define ASC 21
-#define DECLARE 22
-#define DIM 23
-#define DO 24
-#define DOUBLE 25
-#define ELSE 26
-#define END 27
-#define CHR 28
-#define FUNCTION 29
-#define IF 30
-#define INPUT 31
-#define INTEGER 32
-#define LENGTH 33
-#define LOOP 34
-#define PRINT 35
-#define RETURN 36
-#define SCOPE 37
-#define STRING 38
-#define SUBSTR 39
-#define THEN 40
-#define WHILE 41
-#define AND 42
-#define BOOLEAN 43
-#define CONTINUE 44
-#define ELSEIF 45
-#define EXIT 46
-#define FALSE 47
-#define FOR 48
-#define NEXT 49
-#define NOT 50
-#define OR 51
-#define SHARED 52
-#define STATIC 53
-#define TRUE 54
-#define EOL 55
-*/
 
 // Keywords
 /*
