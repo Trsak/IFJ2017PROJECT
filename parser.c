ee127afe9818c7cdee2fe64276e5ad43ec72e116
Tomas Odehnal <xodehn08@stud.fit.vutbr.cz>
2017-11-21 10:05:03 +0100

    AST: Adding string literal into AST.

--- parser.c
+++ parser.c
@@ -101,11 +101,11 @@ void program() {
 
     mainBody();
 
-
+/*
 	for(int i = 0; i < globalStmtArray.length; i++) {
 		if(globalStmtArray.array[i].tag_stmt == function_definition_stmt) {
 			// TODO: segfault if function is not declared but is defined!!
-			//printf("function: %s\n", globalStmtArray.array[i].op.function_definition_stmt.function->data.name);
+			printf("function: %s\n", globalStmtArray.array[i].op.function_definition_stmt.function->data.name);
 			showAruments(globalStmtArray.array[i].op.function_definition_stmt.args);
 		}
 		else if(globalStmtArray.array[i].tag_stmt == function_decl_stmt) {
@@ -116,27 +116,30 @@ void program() {
 			//printf("%s=", globalStmtArray.array[i].op.var_assign_stmt.left->data.name);
 			ast_exp* exp = globalStmtArray.array[i].op.var_assign_stmt.expression;
 			if(exp->op.binaryExp.left != NULL && exp->op.binaryExp.left->tag_exp == bracketExp) {
-				//printf("%s", exp->op.binaryExp.left->op.bracketExp.leftBracket.str);
-				//printf("%d", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.left->op.numberExp);
-				//printf("%s", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.oper.str);
-				//printf("%d", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.right->op.numberExp);
-				//printf("%s", exp->op.binaryExp.left->op.bracketExp.rightBracket.str);
+				printf("%s", exp->op.binaryExp.left->op.bracketExp.leftBracket.str);
+				printf("%d", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.left->op.numberExp);
+				printf("%s", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.oper.str);
+				printf("%d", exp->op.binaryExp.left->op.bracketExp.expression->op.binaryExp.right->op.numberExp);
+				printf("%s", exp->op.binaryExp.left->op.bracketExp.rightBracket.str);
 
-				//printf("%s", exp->op.binaryExp.oper.str);
-				//printf("%d\n", exp->op.binaryExp.right->op.numberExp);
+				printf("%s", exp->op.binaryExp.oper.str);
+				printf("%d\n", exp->op.binaryExp.right->op.numberExp);
 			}
 		}
 		else if(globalStmtArray.array[i].tag_stmt == print_stmt) {
 			ast_exp* exp = globalStmtArray.array[i].op.print_stmt.expression;
 			if(exp->tag_exp == variableExp) {
-				//printf("print %s;\n", exp->op.variableExp->data.name);
+				printf("print %s;\n", exp->op.variableExp->data.name);
 			}
 			else if(exp->tag_exp == integerExp) {
-				//printf("print %d;\n", exp->op.numberExp);
+				printf("print %d;\n", exp->op.numberExp);
+			}
+			else if(exp->tag_exp == stringExp) {
+				printf("print %s\n", exp->op.stringExp.str);
 			}
 		}
-		//printf("\n");
-	}
+		printf("\n");
+	}*/
 
 }
 
